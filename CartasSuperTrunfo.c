3905aebd3ad31e409c1f3fa0f7b3918ecdfe1a16
palmaresfelipe <palmaresfelipe@outlook.com>
2025-08-19 12:08:18 +0000

    Desafio Aventureiro Concluido

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,8 +11,10 @@ int main(){
     float area1;
     float pib1;
     int turisticos1;
+    float densidade1;
+    float pibpercapita1;
 
-    // OBS: O desafio foi construído com base nas informações dos exemplos abaixo.
+    // OBS: O desafio foi construído com base nas informações dos exemplos abaixo. 
 
     /*
     Exemplo:
@@ -44,11 +46,17 @@ int main(){
     scanf("%d", &populacao1);
     printf("Digite a Área em km²:\n");
     scanf("%f", &area1);
-    printf("Digite o PIB em bilhões:\n");
+    printf("Digite o PIB da cidade em bilhões:\n");
     scanf("%f", &pib1);
     printf("Digite o Número de Pontos Turísticos:\n");
     scanf("%d", &turisticos1);
-    printf("\nCarta 1:\nLetra: %c\nCódigo da carta: %s\nCidade: %s\nPopulação: %d habitantes.\nÁrea: %.2f km².\nPIB: %.2f bilhões de reais.\nPontos Turísticos: %d \n\n", estado1, codigo1, cidade1, populacao1, area1, pib1, turisticos1);
+
+    densidade1 = populacao1 / area1;
+    pibpercapita1 = (pib1*1000000000) / populacao1;
+
+
+
+    printf("\nCarta 1:\nLetra: %c\nCódigo da carta: %s\nCidade: %s\nPopulação: %d habitantes.\nÁrea: %.2f km².\nDensidade Populacional: %.2f habitantes por km²\nPIB: %.2f bilhões de reais.\nPIB Per Capita: %.2f reais.\nPontos Turísticos: %d \n\n", estado1, codigo1, cidade1, populacao1, area1, densidade1, pib1, pibpercapita1, turisticos1);
 
 
 
@@ -61,13 +69,17 @@ int main(){
     float area2;
     float pib2;
     int turisticos2;
+    float densidade2;
+    float pibpercapita2;
 
     /*
-    Informações da Carta 2
+    Exemplo 2:
+
+    Carta 2:
     Estado = B
     Código da carta = B01
     Cidade = Aracaju
-    População = 670000 habitantes
+    População = 670000
     Área = 181.90 km²
     PIB = 18.40 bilhões de reais
     Número de pontos Turísticos: 14
@@ -87,14 +99,19 @@ int main(){
     scanf("%d", &populacao2);
     printf("Digite a Área em km²\n");
     scanf("%f", &area2);
-    printf("Digite o PIB em bilhões:\n");
+    printf("Digite o PIB da cidade em bilhões:\n");
     scanf("%f", &pib2);
     printf("Digite o Número de Pontos Turísticos:\n");
     scanf("%d", &turisticos2);
-    printf("\nCarta 2:\nLetra: %c\nCódigo da carta: %s\nCidade: %s \nPopulação: %d habitantes.\nÁrea: %.2f km².\nPIB: %.2f bilhões de reais.\nPontos Turísticos: %d \n", estado2, codigo2, cidade2, populacao2, area2, pib2, turisticos2);
+
+    densidade2 = populacao2 / area2;
+    pibpercapita2 = (pib2*1000000000) / populacao2;
+
+    printf("\nCarta 2:\nLetra: %c\nCódigo da carta: %s\nCidade: %s \nPopulação: %d habitantes.\nÁrea: %.2f km².\nDensidade Populacional: %.2f habitantes por km²\nPIB: %.2f bilhões de reais.\nPIB per capita: %.2f reais.\nPontos Turísticos: %d \n", estado2, codigo2, cidade2, populacao2, area2, densidade2, pib2, pibpercapita2, turisticos2);
 
 
 
     return 0;
+
 
 }
